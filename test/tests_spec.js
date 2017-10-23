13d79fff7427d4c5e1144a77d645d16187dbf2bc
nataliefrancis <natdfrancis@gmail.com>
2017-10-23 10:59:48 -0600

    continues working on mocha tests to get them working

--- test/tests_spec.js
+++ test/tests_spec.js
@@ -5,12 +5,12 @@ const request = require('request');
 var baseURL = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyA8MjE8GAbJYYoJl77bJckRqYvANrVt1D8&callback=initMap';
 
 describe("API test", function () {
-		request(baseURL, function (error, response, body) { 
-			it("should receive a 200/OK HTTP status code", function () {
-				expect(response.statusCode).to.equal(200);
-			});
-			done();
+	request(baseURL, function (error, response, body) { 
+		it("should receive a 200/OK HTTP status code", function () {
+			expect(response.statusCode).to.equal(200);
 		});
+		done();
+	});
 });
 
 describe("Server should be running", function() {
