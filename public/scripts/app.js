29c430fcb5fc0162e54473f3794a4bc7f26ad117
nataliefrancis <natdfrancis@gmail.com>
2017-10-23 11:00:41 -0600

    Changes to delete function of trip cards

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -24,30 +24,87 @@ function initMap() {
     // recenter the map to where the marker was pinned
     map.panTo(latLng);
 
-       //when the marker is clicked, a box opens
-    // google.maps.event.addListener(map, 'click', function(event) {
-    //   placeMarker(map, event.latLng);
-    // });
+        // creating an info window that populates location
+    var infowindow = new google.maps.InfoWindow({
+      content: "I will show a city name"
+      // document.getElementById('city')
+    });
 
-    function placeMarker(map, location) {
-      var marker = new google.maps.Marker({
-        position: location,
-        map: map
+          // when the marker is clicked, the info winder pops up.
+    google.maps.event.addListener(marker, 'click', function() {
+        infowindow.open(map, marker);
       });
-
-      var infowindow = new google.maps.InfoWindow({
-        content: 'Latitude: ' + location.lat() +
-        '<br>Longitude: ' + location.lng()
-        // document.getElementById('city')
-      });
-
-      google.maps.event.addListener(marker, 'click', function() {
-            infowindow.open(map, marker);
-          });
     }
-      
-  }
+}
 
+      // Adds a search box to a map, using the Google Place Autocomplete
+      // feature. People can enter geographical searches. The search box will return a
+      // pick list containing a mix of places and predicted search terms.
+function initAutocomplete() {
+    var map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: -33.8688, lng: 151.2195},
+      zoom: 13,
+      mapTypeId: 'roadmap'
+    });
+
+    // Create the search box and link it to the UI element.
+    var input = document.getElementById('pac-input');
+    var searchBox = new google.maps.places.SearchBox(input);
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+    // Bias the SearchBox results towards current map's viewport.
+    map.addListener('bounds_changed', function() {
+      searchBox.setBounds(map.getBounds());
+    });
+
+    var markers = [];
+    // Listen for the event fired when the user selects a prediction and retrieve
+    // more details for that place.
+    searchBox.addListener('places_changed', function() {
+      var places = searchBox.getPlaces();
+
+      if (places.length == 0) {
+        return;
+      }
+
+      // Clear out the old markers.
+      markers.forEach(function(marker) {
+        marker.setMap(null);
+      });
+      markers = [];
+
+      // For each place, get the icon, name and location.
+      var bounds = new google.maps.LatLngBounds();
+      places.forEach(function(place) {
+        if (!place.geometry) {
+          console.log("Returned place contains no geometry");
+          return;
+        }
+        var icon = {
+          url: place.icon,
+          size: new google.maps.Size(71, 71),
+          origin: new google.maps.Point(0, 0),
+          anchor: new google.maps.Point(17, 34),
+          scaledSize: new google.maps.Size(25, 25)
+        };
+
+    // Create a marker for each place.
+      markers.push(new google.maps.Marker({
+        map: map,
+        icon: icon,
+        title: place.name,
+        position: place.geometry.location
+      }));
+
+      if (place.geometry.viewport) {
+        // Only geocodes have viewport.
+        bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location);
+      }
+    });
+    map.fitBounds(bounds);
+  });
 }
 
 $(document).ready(function() {
@@ -59,6 +116,7 @@ $(document).ready(function() {
     url: '/user',
     success: function(result) {
       userId = result;
+      console.log(userId);
     }
   });
 
@@ -93,9 +151,9 @@ $(document).ready(function() {
     // renders a new trip on the page
   function renderNewTrip(trip) {
     console.log('rendering trip ' +trip._id);
-    userTrips.push(trip);
+    // userTrips.push(trip); not sure I need this anymore
     var tripHtml =
-    "<div class ='col-4' id='onetrip'>"+
+    "<div class ='col-4 onetrip' id="+trip._id+">"+
       // "<i class='fa fa-suitcase' aria-hidden='true'></i>"+
           //List that populates from form
       "<ul class = 'newtripcard' style='list-style-type:none'>"+
@@ -105,13 +163,7 @@ $(document).ready(function() {
         "<li><span class='tripactivities'>Activities: "+trip.activities+"</span></li>"+
       "</ul>"+
           //buttons that populate on each trip card
-      "<div class='btn-group tripbuttons' data-toggle='buttons'>"+
-        // "<label class='btn btn-secondary'>"+
-        //   "<input type='radio' name='edit' id='editbutton' autocomplete='off'> Edit"+
-        // "</label>"+
-        // "<label class='btn btn-secondary'>"+
-        //   "<input type='radio' name='delete' id='deletebutton' data-id='" +trip._id+ "' autocomplete='off'> Delete"+
-        // "</label>"+
+      "<div class='btn-group tripbuttons'>"+
         "<button type='button' id='editbutton' data-id='" +trip._id+ "'>Edit</button>"+
         "<button type='button' id='deletebutton' data-id='" +trip._id+ "'>Delete</button>"+
       "</div>"+
@@ -129,6 +181,7 @@ $(document).ready(function() {
         url: '/userpage/trips/'+id,
         success: deleteTrip
       });
+      return;
     });
 
       //Deletes a book on the front end by looping through their trip array
@@ -136,19 +189,20 @@ $(document).ready(function() {
       console.log(deletedTrip);
       let oneTrip = deletedTrip;
       let tripId = deletedTrip._id;
-      for(let i=0; i< userTrips.length; i++) {
-        if(userTrips[i]._id === tripId) {
-          userTrips.splice(i, 1);
-          removeTripcard();
+      // for(let i=0; i< userTrips.length; i++) {
+      //   if(userTrips[i]._id === tripId) {
+      //     userTrips.splice(i, 1);    not sure I need this anymore
+      removeTripcard(tripId);
           // break;
-        }
-      }
-      function removeTripcard() {
-        $('#onetrip').remove();
-      }
+        // }
+      return;
     }
-    // break;
-   }
+
+      function removeTripcard(tripId) {
+        $('#'+tripId).remove();
+      }
+    // }
+  }
 });
 
 
