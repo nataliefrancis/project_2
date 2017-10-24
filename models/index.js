de2568066e886839d36e0236034d80c0d5475f26
nataliefrancis <natdfrancis@gmail.com>
2017-10-23 18:39:15 -0600

    Heroku take 4

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 var mongoose = require("mongoose");
 // connecting with mongo database
-mongoose.connect(process.env.MONODB_URI || 'mongodb://localhost/wanderlist');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/wanderlist');
 
 module.exports.User = require('./user.js');
 module.exports.Trip = require('./trip.js');
